7df5e872b0c002b34574476382159f495900abf3
Shira Ozeri <shiraozeri3@gmail.com>
2025-07-21 11:26:35 +0300

    Initial clean commit

---
+++ PathUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PathUI : MonoBehaviour
+{
+    public InputField startInput;
+    public InputField endInput;
+    public GridBuilder gridBuilder;
+
+    public void OnCalculatePath()
+    {
+        string[] startParts = startInput.text.Split(',');
+        string[] endParts = endInput.text.Split(',');
+
+        int startX = int.Parse(startParts[0]);
+        int startZ = int.Parse(startParts[1]);
+        int endX = int.Parse(endParts[0]);
+        int endZ = int.Parse(endParts[1]);
+
+        gridBuilder.CalculatePath(startX, startZ, endX, endZ);
+    }
+}
---
+++ RobotController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float rotateSpeed = 100f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float rotate = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, move);
+        transform.Rotate(0, rotate, 0);
+    }
+}
